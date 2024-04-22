e5ac289e1cad6c5110c0748e4668f409cdb3519e
Rousent <86386444+Rousent@users.noreply.github.com>
2024-04-21 20:32:19 -0600

    Update page.tsx

--- src/app/(mainApp)/page.tsx
+++ src/app/(mainApp)/page.tsx
@@ -4,6 +4,7 @@ import { Link } from "@nextui-org/link";
 import { links } from "@/types/links";
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import CameraAccess from "@/components/CameraAccess";
+import { IconCheck } from "@tabler/icons-react";
 
 export default function Home() {
 	return (
@@ -137,6 +138,113 @@ export default function Home() {
 							<li>¡Practica la forma de la seña!</li>
 						</ul>
 					</div>
+				</div>
+			</div>
+
+			<div className="mt-10">
+				<h2>Nuestros planes de precios</h2>
+				<div className="grid grid-cols-3 gap-4">
+					<Card className="p-7">
+						<CardBody>
+							<span className="text-center text-3xl font-bold">
+								Gratuito
+							</span>
+							<span className="text-center mt-2">
+								0.00$ / mes
+							</span>
+							<ul className="mt-6 flex flex-col gap-3">
+								<li className="flex gap-6 items-center text-base">
+									<IconCheck className="shrink-0 w-10 h-10 stroke-naranja" />
+									Acceso libre a las primeras lecciones del
+									nivel Basico.
+								</li>
+								<li className="flex gap-6 items-center text-base">
+									<IconCheck className="shrink-0 w-10 h-10 stroke-naranja" />
+									Prueba el sistema de reconocimiento de señas
+									basico.
+								</li>
+							</ul>
+						</CardBody>
+						<CardFooter className="flex flex-col gap-1">
+							<Button
+								color="default"
+								variant="light"
+								as={Link}
+								href={links.signup}
+							>
+								Empezar ahora
+							</Button>
+						</CardFooter>
+					</Card>
+					<Card className="p-7">
+						<CardBody>
+							<span className="text-center text-3xl font-bold">
+								Premium
+							</span>
+							<span className="text-center mt-2">
+								400.00$ / mes
+							</span>
+							<ul className="mt-6 flex flex-col gap-3">
+								<li className="flex gap-6 items-center text-base">
+									<IconCheck className="shrink-0 w-10 h-10 stroke-naranja" />
+									Acceso total a todas las lecciones de los
+									niveles Basico, Intermedio y Avanzado.
+								</li>
+								<li className="flex gap-6 items-center text-base">
+									<IconCheck className="shrink-0 w-10 h-10 stroke-naranja" />
+									Practica con el sistema de reconocimiento de
+									señas avanzado.
+								</li>
+							</ul>
+						</CardBody>
+						<CardFooter className="flex flex-col gap-1">
+							<Button
+								color="primary"
+								variant="ghost"
+								as={Link}
+								href={links.signup}
+							>
+								Detalles
+							</Button>
+						</CardFooter>
+					</Card>
+					<Card className="p-7">
+						<CardBody>
+							<span className="text-center text-3xl font-bold">
+								Empresarial
+							</span>
+							<span className="text-center mt-2">
+								Personalizado
+							</span>
+							<ul className="mt-6 flex flex-col gap-3">
+								<li className="flex gap-6 items-center text-base">
+									<IconCheck className="shrink-0 w-10 h-10 stroke-accent" />
+									Acceso total a todas las lecciones de los
+									niveles Basico, Intermedio y Avanzado.
+								</li>
+								<li className="flex gap-6 items-center text-base">
+									<IconCheck className="shrink-0 w-10 h-10 stroke-accent" />
+									Acceso total a todas las lecciones de LSM
+									Orientado a Empresas.
+								</li>
+								<li className="flex gap-6 items-center text-base">
+									<IconCheck className="shrink-0 w-10 h-10 stroke-accent" />
+									Practica con el sistema de reconocimiento de
+									señas avanzado.
+								</li>
+							</ul>
+						</CardBody>
+						<CardFooter className="flex flex-col gap-1">
+							<Button
+								color="success"
+								variant="ghost"
+								as={Link}
+								href={links.signup}
+							>
+								Contactanos
+							</Button>
+						</CardFooter>
+					</Card>
 				</div>
 			</div>
 
