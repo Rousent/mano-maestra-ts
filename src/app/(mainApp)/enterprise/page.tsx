ae3bd05b6ded44bae9c492f6ab1b266c36903191
Ukyo Moreno Cancino <146487624+UkyoProfesional@users.noreply.github.com>
2024-05-16 08:02:07 -0600

    Cambio de paginas
    Cambie el error de que about muestre lo de enterprise

--- src/app/(mainApp)/enterprise/page.tsx
+++ src/app/(mainApp)/enterprise/page.tsx
@@ -1,12 +1,71 @@
-import { IconBackhoe } from "@tabler/icons-react";
 
+import { Button } from "@nextui-org/react";
 export default function Empresas() {
 	return (
-		<>
-			<div className="flex flex-col w-full justify-center items-center">
-				<IconBackhoe className="w-40 h-40" />
-				<div className="text-5xl">Sitio en construcción.</div>
+		<div className="flex flex-row px-40 py-10 justify-between gap-10">
+			<div className="flex flex-col w-full gap-5">
+				<h2 className="text-center font-bold text-3xl">
+					Ofrecemos promociones y planes personalizados a empresas.
+				</h2>
+				<h4 className="text-center font-medium text-xl">
+					Llena el formulario e incluye detalles de sus necesidades
+					para llegar al mejor acuerdo.
+				</h4>
+				<img src="img/logo.png" className="p-10" />
 			</div>
-		</>
+			<form className="w-form h-fit rounded-lg border-2 border-black p-10 flex flex-col justify-center items-center bg-fondo">
+				<div className="flex flex-col w-full">
+					<label htmlFor="nombres">Nombre(s)</label>
+					<input id="nombres" placeholder="Ej. Luis Angel" />
+				</div>
+				<div className="flex flex-col w-full">
+					<label>Apellidos</label>
+					<div className="flex flex-row gap-4">
+						<input id="paterno" placeholder="Paterno" />
+						<input id="materno" placeholder="Materno" />
+					</div>
+				</div>
+				<div className="flex flex-col w-full">
+					<label htmlFor="email">
+						Correo electronico empresarial
+					</label>
+					<input id="email" placeholder="Ej. ejemplo@gmail.com" />
+				</div>
+				<div className="flex flex-col w-full">
+					<label htmlFor="empresa">Nombre de la empresa</label>
+					<input id="empresa" placeholder="Nombre de la empresa" />
+				</div>
+				<div className="flex flex-col w-full">
+					<label htmlFor="puesto">Puesto</label>
+					<input
+						id="puesto"
+						placeholder="Cargo dentro de la empresa"
+					/>
+				</div>
+				<div className="flex flex-col w-full">
+					<label htmlFor="telephone">Teléfono</label>
+					<input id="telephone" placeholder="000-000-0000" />
+				</div>
+				<div className="flex flex-col w-full">
+					<label htmlFor="tamaño">Tamaño de la empresa</label>
+					<input
+						id="tamaño"
+						placeholder="Cantidad aproximada de empleados"
+					/>
+				</div>
+				<div className="w-full">
+					<label htmlFor="details">Detalles</label>
+					<br />
+					<textarea
+						id="details"
+						rows={5}
+						className="w-full border-2 border-solid border-black rounded-lg px-3 py-0.5"
+						placeholder="Expliquenos sus necesidades"
+					/>
+				</div>
+				<Button>Enviar</Button>
+			</form>
+		</div>
 	);
+	
 }
